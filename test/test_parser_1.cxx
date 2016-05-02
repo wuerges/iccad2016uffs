891e0ae23a69b0ccc601fa6ddeaf68b1962f8cab
Emilio Wuerges <wuerges@gmail.com>
2016-05-02 18:18:00 -0300

    basic tests working

--- test/test_parser_1.cxx
+++ test/test_parser_1.cxx
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
+#include <boost/spirit/include/support_istream_iterator.hpp>
 #include <verilog_parser.hpp>
 
 int
@@ -13,8 +13,9 @@ main(int nargs, char** argv)
   using boost::spirit::istream_iterator;
 
   std::ifstream input(argv[1]);
+  input.unsetf(std::ios::skipws);
+
   istream_iterator begin(input);
   istream_iterator end;
-  parser::parse_verilog(begin, end);
-  return 0;
+  return parser::parse_verilog(begin, end);
 }
--- test/test_spirit_1.cxx
+++ test/test_spirit_1.cxx
@@ -17,18 +17,28 @@ namespace client
     int parse_number(Iterator first, Iterator last)
     {
         using x3::int_;
+        using x3::char_;
         using x3::phrase_parse;
         using ascii::space;
         using x3::parse;
 
         int value;
-        bool r = phrase_parse(
+        std::vector<int> values;
+        phrase_parse(
             first,                          // Start Iterator
             last,                           // End Iterator
             int_,
             space,                          // The Skip-Parser
             value
         );
+      
+        auto identifier = +char_("a-zA-Z0-9_");
+        std::string t = "aaoeu_123_123";
+        std::string result;
+        parse(t.begin(), t.end(), identifier, result);
+
+        std::cout << "Result: " << result << std::endl;
+
         return value;
     }
 }

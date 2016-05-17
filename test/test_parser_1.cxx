a2c26256b2277462ccb2516c6fdd0531fd89ba51
marcoputon <marcoputon@gmail.com>
2016-05-17 18:19:58 -0300

    Fixed: argumento do teste nao estava sendo usado

--- test/test_parser_1.cxx
+++ test/test_parser_1.cxx
@@ -8,27 +8,16 @@
 #include <vector>
 #include <boost/spirit/include/support_istream_iterator.hpp>
 
-
-int teste(std::string s)
+int
+main(int nargs, char** argv)
 {
-	verilog::ast::Verilog v;
-	using boost::spirit::istream_iterator;
-	
-	std::ifstream input(s);
-	input.unsetf(std::ios::skipws);
-	
-	istream_iterator begin(input);
-	istream_iterator end;
-	return verilog::parser::parse_verilog(v, begin, end);
+  verilog::ast::Verilog v;
+  using boost::spirit::istream_iterator;
+
+  std::ifstream input(argv[1]);
+  input.unsetf(std::ios::skipws);
+
+  istream_iterator begin(input);
+  istream_iterator end;
+  return verilog::parser::parse_verilog(v, begin, end);
 }
-
-int main()
-{
-	std::vector<std::string> s = {"../testcases/TEST/cir1.v", "../testcases/TEST/cir2.v"};
-	for(int i = 0; i < s.size(); i++)
-		teste(s[i]);
-	
-	return 0;
-}
-
-

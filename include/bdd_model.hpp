ef6f35a7ca36729df30fd452ada55d83a129ee74
Emilio Wuerges <wuerges@gmail.com>
2017-11-20 17:39:13 -0200

    basic bdd conjunction ok

--- include/bdd_model.hpp
+++ include/bdd_model.hpp
@@ -41,7 +41,7 @@ namespace verilog
        */
       const int n;
       Node(const int s_, const int r_, const int p_, const int n_): 
-        s(s), r(r), p(p_), n(n_)
+        s(s_), r(r_), p(p_), n(n_)
       {}
       //Node(const int s_): s(s), r(true), p(1), n(0) {}
 
@@ -56,7 +56,7 @@ namespace verilog
 
       int add_simple_input(int s) {
         graph.emplace_back(s, true, 1, 0);
-        return s;
+        return graph.size() - 1;
       }
 
       /*int clone_node(int x) {
@@ -81,9 +81,42 @@ namespace verilog
         if(x == 0) return 0;
         if(y == 0) return 0;
 
-
+        if(graph[x].s == graph[y].s) {
+          int n = conjunction(graph[x].n, graph[y].n);
+          int p = conjunction(graph[x].p, graph[y].p);
+          graph.emplace_back(graph[x].s, false, n, p);
+        }
+        else if(graph[x].s < graph[y].s) {
+          int n = conjunction(x, graph[y].n);
+          int p = conjunction(x, graph[y].p);
+          graph.emplace_back(graph[y].s, false, n, p);
+        }
+        else { // x > y
+          int n = conjunction(y, graph[y].n);
+          int p = conjunction(y, graph[y].p);
+          graph.emplace_back(graph[x].s, false, n, p);
+        }
+        return graph.size() - 1;
       }
 
+
+      friend std::ostream& operator<<(std::ostream & out, const BDD & b) {
+        out << "digraph G {\n";
+        out << "  0 [shape=box,style=filled];\n";
+        out << "  1 [shape=box,style=filled];\n";
+        for(int i = 2; i < b.graph.size(); ++i) {
+          if(b.graph[i].r)
+            out << "  " << i << " [style=filled,label=\""<< b.graph[i].s << ","<< i<< "\"];\n";
+          else 
+            out << "  " << i << " [label=\""<< b.graph[i].s << ","<< i<< " \"];\n";
+        }
+        for(int i = 2; i < b.graph.size(); ++i) {
+          out << "  " << i << "->" << b.graph[i].n << "[style=dotted];\n";
+          out << "  " << i << "->" << b.graph[i].p << "\n";
+        }
+        out << "};\n";
+        return out;
+      }
     };
   }
 }
--- test/test_bdd.cxx
+++ test/test_bdd.cxx
@@ -15,6 +15,15 @@ using namespace std::chrono;
 
 int main(int nargs, char** argv){
 
+
+  BDD bdds;
+  int x = bdds.add_simple_input(5);
+  int y = bdds.add_simple_input(6);
+  printf("//x = %d, y = %d\n", x, y);
+  bdds.conjunction(x, y);
+
+  std::cout << bdds << '\n';
+
   /*
   BDD a("a");
   BDD b("b");

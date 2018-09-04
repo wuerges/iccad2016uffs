e48c7c62dd5741b645b853597f6e0a803b463f8c
Emilio Wuerges <wuerges@gmail.com>
2018-09-04 12:43:24 -0300

    Compiling again.

--- include/bdd_graph_builder.hpp
+++ include/bdd_graph_builder.hpp
@@ -1,17 +1,50 @@
 #pragma once
 
 #include "bdd_model.hpp"
+#include "graph_algorithm.hpp"
 #include "graph_model.hpp"
+#include <vector>
 
 namespace verilog
 {
 
-using namespace bdd;
-using namespace graph;
+//using namespace bdd;
+//using namespace graph;
 
-void build(BDD & bdds, G & graph) {
-  
+void build(bdd::BDD & bdds, graph::G & g) {
+  auto topo_order = topological_sort(g);
+
+  std::vector<bdd::Node*> bdd_nodes(num_vertices(g.graph));
+
+  for (int node : topo_order) {
+    if(in_degree(node, g.graph) == 0) {
+      bdd_nodes[node] = bdds.add_simple_input(g.graph[node].identifier);
+    }
+    else if(node == g.zero) {
+      bdd_nodes[node] = bdds.zero;
+    }
+    else if(node == g.one) {
+      bdd_nodes[node] = bdds.one;
+    }
+    else {
+      auto p = in_edges(node, g.graph);
+      bdd::Node * sum = bdds.one;
+
+      for (auto e = p.first; e != p.second; ++e) {
+        bdd::Node * s = bdd_nodes[source(*e, g.graph)];
+
+        bdd::Node * s2 = g.graph[*e] == NegP::Positive ? s : bdds.negate(s);
+
+        sum = bdds.land(sum, s);
+
+        // return np == NegP::Positive ? v : !v;
+        // LogicValue value = propagate(g.graph[s].value, g.graph[*e]);
+
+      }
+
+      bdd_nodes[node] = sum;
+    }
+  }
 }
-
 
 }
--- test/test_bdd.cxx
+++ test/test_bdd.cxx
@@ -2,21 +2,21 @@
 #include "bdd_graph_builder.hpp"
 
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <map>
-#include <chrono>
+// #include <fstream>
+// #include <string>
+// #include <vector>
+// #include <map>
+// #include <chrono>
 
-#include <boost/graph/graph_traits.hpp>
-#include <boost/graph/adjacency_list.hpp>
-
-using namespace verilog::bdd;
-using namespace std::chrono;
+// #include <boost/graph/graph_traits.hpp>
+// #include <boost/graph/adjacency_list.hpp>
+//
+using verilog::bdd::BDD;
+using verilog::bdd::Node;
+// using namespace std::chrono;
+// using namespace std;
 
 int main(int nargs, char** argv){
-
-
   BDD bdds;
   Node * x = bdds.add_simple_input("x");
   Node * y = bdds.add_simple_input("y");
